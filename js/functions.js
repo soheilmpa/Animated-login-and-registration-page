bb85a71f9aa7ff359214e4412782aae8d41af749
Soheil <soheilmohammadpor13@gmail.com>
2021-07-26 01:57:25 +0430

    js file

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+var carf = document.getElementById("card")
+
+
+
+function openRegister(){
+    card.style.transform = "rotateY(-180deg)"
+}
+
+
+function openLogin(){
+    card.style.transform = "rotateY(0deg)"
+}
